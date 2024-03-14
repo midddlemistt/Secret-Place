27b8d0512be47239cd3f7507e0275bb7c924555a
me <pashatimofeeff@gmail.com>
2024-03-14 16:15:28 +0300

    Initial Commit

---
+++ Secret Place/Secret_PlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Secret_PlaceApp.swift
+//  Secret Place
+//
+//  Created by 123 on 14.03.24.
+//
+
+import SwiftUI
+
+@main
+struct Secret_PlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
